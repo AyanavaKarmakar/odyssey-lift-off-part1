0588589c2e86abee2da0481e03c174cd821f2399
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-12-18 19:23:36 +0530

    wrap in query result component

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Layout } from '../components';
 import { useQuery, gql } from "@apollo/client";
 import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
 
 /**
  * Tracks Page is the Catstronauts home page.
@@ -28,14 +29,12 @@ const Tracks = () => {
 
   const { loading, error, data } = useQuery(TRACKS);
 
-  if (loading) return 'Loading...';
-
-  if (error) return `Error! ${error.message}`;
-
   return <Layout grid>
-    {data?.tracksForHome?.map(track => (
-      <TrackCard key={track.id} track={track} />
-    ))}
+    <QueryResult error={error} loading={loading} data={data}>
+      {data?.tracksForHome?.map(track => (
+        <TrackCard key={track.id} track={track} />
+      ))}
+    </QueryResult>
   </Layout>;
 };
 
